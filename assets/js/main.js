4d188bc235b4a14f42e306efa213ba41482739d5
KarinAAC <karin_alejo@hotmail.com>
2017-04-11 14:08:48 -0500

    subiendo segundo avance

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+function spanVisto1(){
+  document.getElementById('al1').style.display='block';
+}
+
+function spanOculto1(){
+  document.getElementById('al1').style.display='none';
+}
+
+function spanVisto2(){
+  document.getElementById('al2').style.display='block';
+}
+
+function spanOculto2(){
+  document.getElementById('al2').style.display='none';
+}
+function spanVisto3(){
+  document.getElementById('al3').style.display='block';
+}
+
+function spanOculto3(){
+  document.getElementById('al3').style.display='none';
+}
+function spanVisto4(){
+  document.getElementById('al4').style.display='block';
+}
+
+function spanOculto4(){
+  document.getElementById('al4').style.display='none';
+}
+function spanVisto5(){
+  document.getElementById('al5').style.display='block';
+}
+
+function spanOculto5(){
+  document.getElementById('al5').style.display='none';
+}
+function spanVisto6(){
+  document.getElementById('al6').style.display='block';
+}
+
+function spanOculto6(){
+  document.getElementById('al6').style.display='none';
+}
+
+function spanVisto7(){
+  document.getElementById('al7').style.display='block';
+}
+
+function spanOculto7(){
+  document.getElementById('al7').style.display='none';
+}
+
+function spanVisto8(){
+  document.getElementById('al8').style.display='block';
+}
+
+function spanOculto8(){
+  document.getElementById('al8').style.display='none';
+}
+function spanVisto9(){
+  document.getElementById('al9').style.display='block';
+}
+
+function spanOculto9(){
+  document.getElementById('al9').style.display='none';
+}
